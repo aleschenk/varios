f648f6b3f9501ea22e817d1738f04b74d3033905
Alejandro Schenk <ale.schenk@gmail.com>
2018-04-07 22:53:33 -0300

    update

--- src/main/scala/com/image/ImageMain.scala
+++
@@ -1,9 +0,0 @@
-package com.image
-
-
-object ImageMain {
-
-  def main(args: Array[String]): Unit = {
-    println("Hola Mundo !")
-  }
-}
--- src/main/scala/com/scala/MainTEst.scala
+++
@@ -1,5 +0,0 @@
-package com.scala
-
-class MainTEst {
- //-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005
-}
